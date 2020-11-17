d0ce30899e180d3c83bdefcc6c301c12be92270b
Jesus <berriojesus122@gmail.com>
2020-11-17 08:33:07 -0500

    list of categoties done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-ReactDOM.render(<h1>Seguimos</h1>, document.getElementById('app'))
+import App from './app'
+ReactDOM.render(<App />, document.getElementById('app'))
