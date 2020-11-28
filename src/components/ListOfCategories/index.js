c9e92ee61bdc3653926ca76a48b32069c536a2cf
Jesus <berriojesus122@gmail.com>
2020-11-28 11:45:42 -0500

    add hooks done

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    window.fetch('https://petgram-vercel-ten.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
-        categories.map(
-          category =>
-            <Item key={category.id}>
-              <Category {...category} />
-            </Item>
-        )
+        categories.map(category =>
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
