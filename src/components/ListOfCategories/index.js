3bb673c24178b4d9842ecac40f44bcfe95654c63
Jesus <berriojesus122@gmail.com>
2020-12-01 13:12:04 -0500

    like añadido y uso del local storage

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,19 +1,28 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
-  const [showFixed, setShowFixed] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   useEffect(function () {
+    setLoading(true)
     window.fetch('https://petgram-vercel-ten.vercel.app/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
+        setLoading(false)
       })
   }, [])
+
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     const onScroll = e => {
@@ -22,16 +31,16 @@ export const ListOfCategories = () => {
     }
 
     document.addEventListener('scroll', onScroll)
+
     return () => document.removeEventListener('scroll', onScroll)
   }, [showFixed])
 
   const renderList = (fixed) => (
     <List fixed={fixed}>
       {
-        categories.map(category =>
-          <Item key={category.id}>
-            <Category {...category} />
-          </Item>)
+        loading
+          ? <Item key='loading'><Category /></Item>
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
