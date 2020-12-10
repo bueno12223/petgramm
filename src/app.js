a520393490cbca1023b151ea23ac4fbcc4c5908f
Jesus <berriojesus122@gmail.com>
2020-12-10 09:44:11 -0500

    photo mwhit query done

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
+/* eslint-disable react/jsx-closing-tag-location */
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './containers/photoCardWithQuery'
 import Logo from './components/Logo'
-export const App = () => (
-  <div>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2} />
-  </div>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={detailId} />
+          </>
+      }
+    </div>
+  )
+}
 export default App
