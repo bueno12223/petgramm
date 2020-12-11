871b6f4761cbcaa4a57fba8ab9f5067145c1854e
Jesus <berriojesus122@gmail.com>
2020-12-11 04:13:40 -0500

    togle like in db added

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { Article, ImgWrapper, Img } from './styles'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
+
+import { FavButton } from '../FavButton'
+import { useMuationToogleLike } from '../../hooks/useMuationToogleLike'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -10,9 +12,16 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
-
+  const { mutation, mutationLoading, mutationError } = useMuationToogleLike()
+  const handleFavClick = () => {
+    !liked && mutation({
+      variables: {
+        input: { id }
+      }
+    })
+    setLiked(!liked)
+  }
+  console.log({ mutation, mutationLoading, mutationError })
   return (
     <Article ref={element}>
       {
@@ -24,9 +33,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               </ImgWrapper>
             </a>
 
-            <Button onClick={() => setLiked(!liked)}>
-              <Icon size='32px' /> {likes} likes!
-            </Button>
+            <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
           </>
       }
     </Article>
